e43afb1f5c164485af60764b59533c77bed50bf9
hamail <mhamail1223@gmail.com>
2025-02-28 09:55:50 +0500

    ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-"use client";
-import React from "react";
-import MyTable from "@/components/table";
-import { demoData, demoColumns } from "@/components/table/columns/demo";
-import { demoActionMenuList } from "@/components/table/headerActionList/demo";
-import Topbar from "@/components/topbar";
+'use client';
+import React from 'react';
+import MyTable from '@/components/table';
+import { demoData, demoColumns } from '@/components/table/columns/demo';
+import { demoActionMenuList } from '@/components/table/headerActionList/demo';
+import Topbar from '@/components/topbar';
 const page = () => {
   const ExpandingTable = ({ row, index, data }: any) => {
-    return <div></div>;
+    return <div>sasasdasd</div>;
   };
   return (
     <div className="m-20">
@@ -15,6 +15,7 @@ const page = () => {
         data={demoData}
         columns={demoColumns}
         actionMenuList={demoActionMenuList}
+        expandable={true}
         multiExpandable={true}
         expandingContent={ExpandingTable}
       />
